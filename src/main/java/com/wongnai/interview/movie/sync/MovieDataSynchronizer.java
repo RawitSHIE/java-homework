20c7c8d01b86935fc0b6ce844bbe10bbdd7b4608
lieselottezz <59070141@it.kmitl.ac.th>
2019-04-10 23:09:06 +0700

    Add 3rd assignment, implement MovieDataSynchronizer for synchronization data and HSQLDB, implement search method in DatabaseMovieSearchService, edit the related files for ignoring the case-sensitive event

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,11 +2,18 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MovieData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +26,33 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+
+		Connection con = getConnection();
+		if (con != null) {
+			initData();
+		}
+	}
+
+	// Get connection from in-memory database
+	private Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName("org.hsqldb.jdbc.JDBCDriver");
+			con = DriverManager.getConnection("jdbc:hsqldb:mem:movies;sql.ignore_case=true", "SA", "");
+			System.out.println("HSQLDB Connected");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	// Remove outdated data and retrieve new movie data from movieDataService
+	private void initData() {
+		ArrayList<MovieData> moviesData = movieDataService.fetchAll();
+		movieRepository.deleteAll();
+		for (MovieData movie : moviesData) {
+			Movie temp = new Movie(movie.getTitle(), movie.getCast());
+			movieRepository.save(temp);
+		}
 	}
 }
